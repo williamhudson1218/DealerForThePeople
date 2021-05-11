5275ac403638b6609cb936223ed3c7f2f64e4091
William Hudson <william.hudson@tecuity.com>
2021-05-11 11:18:45 -0600

    Add project files.

---
+++ DealerForThePeople/Helpers.cs
@@ -0,0 +1,60 @@
+﻿using DealerForThePeople.Model;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealerForThePeople
+{
+    public class Helpers
+    {
+        public static void PrintReviewsToConsole(List<Review> reviews)
+        {
+            reviews = reviews.OrderByDescending(x => x.Score).ToList();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(reviews[i].ToString());
+            }
+        }
+
+        public static void GetReviewsFromHtml(HtmlDocument html, List<Review> reviews)
+        {
+            HtmlNodeCollection reviewsHtml = html.DocumentNode.SelectNodes("//div[contains(@class, 'review-entry')]");
+
+            foreach (HtmlNode review in reviewsHtml)
+            {
+                HtmlDocument reviewHtml = new();
+                reviewHtml.LoadHtml(review.OuterHtml);
+                reviews.Add(ParseReview(reviewHtml));
+            }
+        }
+
+        public static Review ParseReview(HtmlDocument reviewHtml)
+        {
+            try
+            {
+                return new Review()
+                {
+                    Username = reviewHtml.DocumentNode.SelectSingleNode("//span[contains(@class, 'font-18')]").InnerText.Replace("- ", ""),
+                    Title = reviewHtml.DocumentNode.SelectSingleNode("//h3[contains(@class, 'no-format')]").InnerText,
+                    Body = reviewHtml.DocumentNode.SelectSingleNode("//p[contains(@class, 'review-content')]").InnerText,
+                    Rating = ParseRating(reviewHtml.DocumentNode.SelectSingleNode("//div[contains(@class, 'rating-static')]").OuterHtml),
+                    Date = DateTime.Parse(reviewHtml.DocumentNode.SelectSingleNode("//div[contains(@class, 'review-date')]")?.ChildNodes[1].InnerText)
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static int ParseRating(string outerHtml)
+        {
+            int indexEnd = outerHtml.IndexOf("pull-right") - 1;
+            _ = int.TryParse(outerHtml.Substring(indexEnd - 2, 2), out int rating);
+            return rating;
+        }
+    }
+}
---
+++ DealerForThePeople/Model/Review.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealerForThePeople.Model
+{
+    public class Review
+    {
+        public string Username { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int Rating { get; set; }
+        public DateTime Date { get; set; }
+        public List<string> EmployeesWorkedWith { get; set; }
+        public int Score
+        {
+            get
+            {
+                return GetScore();
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Username: {Username}");
+            sb.Append(Environment.NewLine); 
+            sb.Append($"Date: {Date.ToString("MM/dd/yyyy")}");
+            sb.Append(Environment.NewLine);
+            sb.Append($"Rating: {Rating}/5");
+            sb.Append(Environment.NewLine);
+            sb.Append($"Title: {Title}");
+            sb.Append(Environment.NewLine);
+            sb.Append($"Body: {Body}");
+            sb.Append(Environment.NewLine);
+            sb.Append($"Score: {Score}");
+            sb.Append(Environment.NewLine);
+            sb.Append(Environment.NewLine);
+            sb.Append("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            sb.Append(Environment.NewLine);
+            return sb.ToString();
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            //2 points for every star
+            score += Rating / 5;
+            if( Body != null)
+            {
+                //1 point for every !, maximum of 3 points
+                score += Body.Count(x => x == '!') < 3 ? Body.Count(x => x == '!') : 3;
+                //2 point for having the word 'love'
+                score += Body.ToLower().Contains("love") ? 2 : 0;
+                //2 point for having the word 'excellent'
+                score += Body.ToLower().Contains("excellent") ? 2 : 0;
+                //2 point for having the word 'fantastic'
+                score += Body.ToLower().Contains("fantastic") ? 2 : 0;
+                //1 point for every 100 characters
+                score += Body.Length / 100;
+            }
+            return score;
+        }
+    }
+}
